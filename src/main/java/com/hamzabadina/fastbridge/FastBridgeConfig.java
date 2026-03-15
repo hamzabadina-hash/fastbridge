6b9af2391e17840e40094af85274d14901fd01ba
hamzabadina-hash <hamzabadina@gmail.com>
2026-03-15 19:42:16 +0000

    Update FastBridgeConfig.java

--- src/main/java/com/hamzabadina/fastbridge/FastBridgeConfig.java
+++ src/main/java/com/hamzabadina/fastbridge/FastBridgeConfig.java
@@ -1,7 +1,7 @@
 package com.hamzabadina.fastbridge;
 
 public class FastBridgeConfig {
-    public static int speedTicks = 2; // default fast now
+    public static int speedTicks = 3;
 
     public static void load() {}
 }
