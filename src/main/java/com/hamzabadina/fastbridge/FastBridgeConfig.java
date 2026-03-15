bcf2e3246e8f55bdc02659b080cdbcafc525212d
hamzabadina-hash <hamzabadina@gmail.com>
2026-03-15 18:37:46 +0000

    Create FastBridgeConfig.java

---
+++ src/main/java/com/hamzabadina/fastbridge/FastBridgeConfig.java
@@ -0,0 +1,9 @@
+package com.hamzabadina.fastbridge;
+
+public class FastBridgeConfig {
+    public static int speedTicks = 4; // 2=fast, 4=medium, 6=slow
+
+    public static void load() {
+        // can be extended to save/load from file
+    }
+}
